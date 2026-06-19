473a336e4f54096dcf8062b9ab8a9efc73a8ca32
Alejandro Mallea <janoma@gmail.com>
2026-06-19 09:57:46 -0700

    add shadcn and clean main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from "react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <section id="center">
-      <div>
-        <h1>Get started</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-        </p>
-      </div>
-      <button
-        type="button"
-        className="counter"
-        onClick={() => { setCount((count) => count + 1); }}
-      >
-        Count is {count}
-      </button>
-    </section>
+    <div className="prose darK:prose-invert max-w-6xl mx-auto border rounded-lg p-4 my-4 bg-white">
+      <header>
+        <h1>Contrast Checker</h1>
+      </header>
+      <main></main>
+    </div>
   );
 }
 
