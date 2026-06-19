49081b02678e7630c4fdb693db03730501634bf1
Alejandro Mallea <janoma@gmail.com>
2026-06-19 16:02:06 -0700

    switch to preact for reduced bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
+import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { defineConfig } from "vite";
 
-import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), preact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
