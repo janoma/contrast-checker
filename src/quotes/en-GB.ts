9a5a29de6ae09f0a2f192e79cfc987c1694b582c
Alejandro Mallea <janoma@gmail.com>
2026-06-20 12:09:48 -0700

    add some extra sentences in english

--- src/quotes/en-GB.ts
+++ src/quotes/en-GB.ts
@@ -2,6 +2,21 @@ const quotes = [
   "We are all of us stars, and we deserve to twinkle.",
   "To infinity and beyond!",
   "Some people are worth melting for.",
+  "When you play the game of thrones, you win or you die.",
+  "I pledge my life and honour to the Night's Watch, for this night and all the nights to come.",
+  "The night is dark and full of terrors.",
+  "Do or do not. There is no try.",
+  "May the Force be with you.",
+  "I find your lack of faith disturbing.",
+  "Life, uh... finds a way.",
+  "God creates dinosaurs, God destroys dinosaurs, God creates Man, Man destroys God, Man creates dinosaurs.",
+  "Your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+  "I'm gonna make him an offer he can't refuse.",
+  "Leave the gun. Take the cannoli.",
+  "A man who doesn't spend time with his family can never be a real man.",
+  "To infinity and beyond!",
+  "You've got a friend in me.",
+  "Reach for the sky!",
 ];
 
 export default quotes;
--- src/quotes/en.ts
+++ src/quotes/en.ts
@@ -1,7 +1,22 @@
 const quotes = [
-  "I will not say 'do not weep,' for not all tears are an evil.",
+  "I will not say do not weep, for not all tears are an evil.",
   "Not all those who wander are lost.",
   "Even the smallest person can change the course of the future.",
+  "When you play the game of thrones, you win or you die.",
+  "I pledge my life and honor to the Night's Watch, for this night and all the nights to come.",
+  "The night is dark and full of terrors.",
+  "Do or do not. There is no try.",
+  "May the Force be with you.",
+  "I find your lack of faith disturbing.",
+  "Life, uh... finds a way.",
+  "God creates dinosaurs, God destroys dinosaurs, God creates Man, Man destroys God, Man creates dinosaurs.",
+  "Your scientists were so preoccupied with whether or not they could, they didn't stop to think if they should.",
+  "I'm gonna make him an offer he can't refuse.",
+  "Leave the gun. Take the cannoli.",
+  "A man who doesn't spend time with his family can never be a real man.",
+  "To infinity and beyond!",
+  "You've got a friend in me.",
+  "Reach for the sky!",
 ];
 
 export default quotes;
